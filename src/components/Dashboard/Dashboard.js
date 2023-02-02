50a07eb09f3344266b70682313f4f663a77d1768
blueqwertz <timlimlei@icloud.com>
2023-02-02 14:45:19 +0100

    ui changes

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,8 +3,8 @@ import SideBar from "./Sidebar"
 
 export default function Dashboard() {
     return (
-        <main>
-            <div className="bg-gray-100 dark:bg-black flex flex-col min-h-screen">
+        <main className="bg-gray-100 dark:bg-black flex flex-col">
+            <div className="flex flex-col min-h-screen max-w-3xl min-w-[70%] self-center">
                 <SideBar />
                 <div className="p-4 mt-10">
                     <h1 className="dark:text-white text-4xl"></h1>
--- src/components/Dashboard/Sidebar.js
+++ src/components/Dashboard/Sidebar.js
@@ -4,7 +4,7 @@ import logo from "../../img/yin-yang-symbol.png"
 const SideBar = () => {
     return (
         <div className="w-full h-16 bg-black flex items-center justify-between p-4 text-gray-50 md:px-10">
-            <div className=" text-2xl font-semibold">Journal 📔</div>
+            <div className=" text-2xl font-semibold">Journal 📕</div>
             <div>
                 <a href="/logout" className="hover:underline hover:cursor-pointer">
                     Logout
