5477be8e076d1605fe365ec11bdf012a559bc558
blueqwertz <timlimlei@icloud.com>
2023-03-08 00:36:55 +0100

    fixed bug

--- client/src/components/DarkModeToggle.js
+++ client/src/components/DarkModeToggle.js
@@ -26,6 +26,18 @@ const DarkModeToggle = () => {
 	}
 
 	useEffect(() => {
+		if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+			document.body.classList.add("dark")
+			document.body.classList.remove("light")
+		} else if (localStorage.theme === "light") {
+			document.body.classList.remove("dark")
+			document.body.classList.add("light")
+		} else if (localStorage.theme === "auto") {
+			document.body.classList.add(window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
+			document.body.classList.remove(window.matchMedia("(prefers-color-scheme: dark)").matches ? "light" : "dark")
+		} else {
+			document.body.classList.add("dark")
+		}
 		function handleClickOutside(event) {
 			if (!event.target.closest("#darkmodetoggle")) {
 				setShowOptions(false)
